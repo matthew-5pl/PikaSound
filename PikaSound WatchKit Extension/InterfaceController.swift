34df861a48ad5692cecea778152fdfc4f79cb691
Matteo Forlani <matteofo06@gmail.com>
2020-12-22 09:23:40 +0100

    Add more sounds

--- PikaSound WatchKit Extension/InterfaceController.swift
+++ PikaSound WatchKit Extension/InterfaceController.swift
@@ -7,10 +7,59 @@
 
 import WatchKit
 import Foundation
+import AVFoundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    var audPl: AVAudioPlayer?
+    @IBAction func audPl4() {
+        let path = Bundle.main.path(forResource: "pikachu0", ofType: "mp3")!
+        do {
+            audPl = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+        }
+        
+        catch {
+            print(error)
+        }
+        audPl?.play()
+    }
+    @IBAction func audPl3() {
+        let path = Bundle.main.path(forResource: "Pikachu", ofType: "mp3")!
+        do {
+            audPl = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+        }
+        
+        catch {
+            print(error)
+        }
+        audPl?.play()
+    }
+    @IBAction func audPl2() {
+        let path = Bundle.main.path(forResource: "pikasc", ofType: "mp3")!
+        do {
+            audPl = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+        }
+        
+        catch {
+            print(error)
+        }
+        audPl?.play()
+    }
+    @IBAction func PikaBtn() {
+        let path = Bundle.main.path(forResource: "pika", ofType: "mp3")!
+        do {
+            audPl = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            
+        }
+        
+        catch {
+            print(error)
+        }
+        audPl?.play()
+    }
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
